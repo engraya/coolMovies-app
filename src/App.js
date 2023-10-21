6682db52cf6a3cabd4fab23cfa335789a205e8d1
Engraya <ahmadyakubuaya@gmail.com>
2023-10-21 17:44:25 +0100

    Cool Movies Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { useEffect, useState } from 'react';
+import searchIcon from './search.svg'
+import MovieComponent from './MovieComponent';
+import tvIcon from './tv.svg'
+
+
+const apiURL = 'https://www.omdbapi.com?apikey=90729ca8&'
+
+function App() { 
+
+  const [movies, setMovies ] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const searchForMovies = async (title) => {
+    const response = await fetch(`${apiURL}&S=${title}`)
+    const data = await response.json();
+   
+    setMovies(data.Search)
+  }
+
+  useEffect(() => {
+    searchForMovies();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Cool <img src={tvIcon} alt="" /> Movies</h1>
+      <div className='search'>
+        <input 
+        placeholder='Search for Available movies'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+         />
+         <img 
+          src={searchIcon}
+          alt="Search"
+          onClick={() => searchForMovies(searchTerm)}
+          />
+      </div>
+
+      {
+        movies.length > 0 
+          ?  (
+            <div className='container'>
+            {
+              movies.map((movie) => (
+                <MovieComponent movie={movie} />
+              ))
+            }           
+            </div>
+          ) :  (
+            <div className='empty'>
+              <h1>No Movies Availabe!</h1>
+            </div>
+          )
+
+      }
+
+
     </div>
+    
   );
 }
 
---
+++ src/MovieComponent.jsx
@@ -0,0 +1,23 @@
+
+const MovieComponent = ({ movie }) => {
+    return (  
+        <div className='movie'>
+                <div>
+                    <h5>{movie.Type}</h5>
+                </div>
+
+                <div>
+                    <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
+                </div>
+                <div>
+                  
+                    <h3>{movie.Title}</h3>
+                    <h5>{movie.Year}</h5>
+           
+                </div>
+    
+        </div>
+    );
+}
+ 
+export default MovieComponent;
